15d248a6418a29ad082794294046e06db533e3da
Cyril Plisko <cyril.plisko@mountall.com>
2016-02-15 16:12:34 +0200

    Compiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,100 @@
+
+extern crate walkdir;
+
+use std::ffi::OsString;
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+use walkdir::{DirEntry, WalkDir, WalkDirIterator};
+
+struct CargoMultiCmd {
+    command: Command,
+    banner: String,
+    top_directory: PathBuf,
+    directories: Vec<PathBuf>,
+}
+
+impl CargoMultiCmd {
+    fn new(cmd: &str, args: Vec<String>) -> Self {
+
+        let mut command = Command::new(cmd);
+        let mut banner = String::from("Executing ");
+        banner.push_str(cmd);
+        for arg in args {
+            command.arg(OsString::from(&arg));
+            banner = banner + " " + &arg;
+        }
+
+        CargoMultiCmd {
+            command: command,
+            banner: banner,
+            top_directory: PathBuf::new(),
+            directories: Vec::new(),
+        }
+    }
+
+    fn set_top_dir(&mut self, dir: PathBuf) -> &mut Self {
+        self.top_directory = dir;
+        self
+    }
+
+    fn collect_crate_candidates(&mut self) -> &mut Self {
+        self.directories = WalkDir::new(&self.top_directory)
+                               .min_depth(1)
+                               .max_depth(1)
+                               .into_iter()
+                               .filter_entry(|e| is_crate(e))
+                               .map(|e| e.ok().unwrap().path().to_path_buf())
+                               .collect::<Vec<_>>();
+        self
+    }
+
+    fn announce(&self) -> &Self {
+        println!("Executing cargo {}", self.banner);
+        self
+    }
+
+    fn dispatch(&mut self) -> &Self {
+        for dir in &self.directories {
+            println!("{}:", dir.display());
+            let out = self.command
+                          .current_dir(dir)
+                          .output()
+                          .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+            if out.status.success() {
+                println!("{}",
+                         ident_text(String::from_utf8_lossy(&out.stdout).into_owned()));
+            } else {
+                println!("{}",
+                         ident_text(String::from_utf8_lossy(&out.stderr).into_owned()));
+            }
+            println!("");
+        }
+        self
+    }
+}
+
+fn ident_text(text: String) -> String {
+    let mut out = String::new();
+
+    for line in text.lines() {
+        out.push_str("        ");
+        out.push_str(line);
+    }
+    out
+}
+
+fn is_crate(entry: &DirEntry) -> bool {
+    entry.path().join("Cargo.toml").exists()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().skip(1).collect();
+    let cwd = env::current_dir().unwrap().join("..");
+    let mut multi = CargoMultiCmd::new("cargo", args);
+
+    multi.set_top_dir(cwd);
+    multi.collect_crate_candidates();
+    multi.announce();
+    multi.dispatch();
 }
