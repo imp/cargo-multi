3f1ed08ae211bdafca6bd322c395f0e9323f090e
Cyril Plisko <cyril.plisko@mountall.com>
2016-02-18 00:23:35 +0200

    Refactor into builder

--- src/main.rs
+++ src/main.rs
@@ -3,81 +3,92 @@ extern crate walkdir;
 use std::ffi::OsString;
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Output};
+use std::vec::IntoIter;
 use walkdir::{DirEntry, WalkDir, WalkDirIterator};
 
 struct MultiCmd {
+    cmd: String,
+    args: Vec<String>,
+    min_depth: usize,
+    max_depth: usize,
+    root: PathBuf,
+}
+
+struct MultiCmdRunner {
     command: Command,
-    banner: String,
-    top_directory: PathBuf,
-    directories: Vec<PathBuf>,
+    directories: IntoIter<PathBuf>,
 }
 
 impl MultiCmd {
     fn new(cmd: &str, args: Vec<String>) -> Self {
 
-        let mut command = Command::new(cmd);
-        let mut banner = String::from("Executing ") + &cmd;
-        for arg in args {
+        MultiCmd {
+            cmd: String::from(cmd),
+            args: args,
+            min_depth: 1,
+            max_depth: 1,
+            root: PathBuf::new(),
+        }
+    }
+
+    fn rootdir(&mut self, dir: PathBuf) -> &mut Self {
+        self.root = dir;
+        self
+    }
+
+    fn build(&self) -> MultiCmdRunner {
+        let mut command = Command::new(&self.cmd);
+        let mut banner = String::from("Executing ") + &self.cmd;
+        for arg in &self.args {
             command.arg(OsString::from(&arg));
             banner = banner + " " + &arg;
         }
+        let dirs = WalkDir::new(&self.root)
+                       .min_depth(self.min_depth)
+                       .max_depth(self.max_depth)
+                       .into_iter()
+                       .filter_entry(|e| is_crate(e))
+                       .map(|e| e.ok().unwrap().path().to_path_buf())
+                       .collect::<Vec<_>>();
 
-        MultiCmd {
+        announce(&banner);
+
+        MultiCmdRunner {
             command: command,
-            banner: banner,
-            top_directory: PathBuf::new(),
-            directories: Vec::new(),
+            directories: dirs.into_iter(),
         }
-    }
-
-    fn set_top_dir(&mut self, dir: PathBuf) -> &mut Self {
-        self.top_directory = dir;
-        self
-    }
-
-    fn collect_crate_candidates(&mut self) -> &mut Self {
-        self.directories = WalkDir::new(&self.top_directory)
-                               .min_depth(1)
-                               .max_depth(1)
-                               .into_iter()
-                               .filter_entry(|e| is_crate(e))
-                               .map(|e| e.ok().unwrap().path().to_path_buf())
-                               .collect::<Vec<_>>();
-        self
-    }
-
-    fn announce(&self) -> &Self {
-        let mut line = String::new();
-        for _ in 0..self.banner.len() {
-            line.push('-');
-        }
-        println!("{}", line);
-        println!("{}", self.banner);
-        println!("{}", line);
-        self
-    }
-
-    fn dispatch(&mut self) -> &Self {
-        for dir in &self.directories {
-            println!("{}:", dir.display());
-            let out = self.command
-                          .current_dir(dir)
-                          .output()
-                          .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
-            if out.status.success() {
-                print_ident(out.stdout);
-            } else {
-                print_ident(out.stderr);
-            }
-            println!("");
-        }
-        self
     }
 }
 
-fn print_ident(buf: Vec<u8>)
-{
+impl MultiCmdRunner {
+    fn run(&mut self, dir: PathBuf) -> Output {
+        println!("{}:", dir.display());
+        self.command.current_dir(dir).output().unwrap()
+    }
+}
+
+impl Iterator for MultiCmdRunner {
+    type Item = Output;
+    fn next(&mut self) -> Option<Output> {
+        match self.directories.next() {
+            Some(p) => Some(self.run(p)),
+            None => None,
+        }
+    }
+}
+
+fn announce(banner: &str) {
+    let mut line = String::new();
+    for _ in 0..banner.len() {
+        line.push('-');
+    }
+    println!("{}", line);
+    println!("{}", banner);
+    println!("{}", line);
+}
+
+fn print_ident(buf: Vec<u8>) {
     // ::<'a>(v: &'a [u8])
     for line in String::from_utf8_lossy(&buf[..]).lines() {
         println!("        {}", line);
@@ -91,10 +102,17 @@ fn is_crate(entry: &DirEntry) -> bool {
 fn main() {
     let args = env::args().skip(2).collect();
     let cwd = env::current_dir().unwrap();
-    let mut multi = MultiCmd::new("cargo", args);
+    let multi = MultiCmd::new("cargo", args)
+                        .rootdir(cwd)
+                        .build();
 
-    multi.set_top_dir(cwd);
-    multi.collect_crate_candidates();
-    multi.announce();
-    multi.dispatch();
+    for result in multi {
+        if result.status.success() {
+            print_ident(result.stdout);
+        } else {
+            print_ident(result.stderr);
+        }
+        println!("");
+
+    }
 }
