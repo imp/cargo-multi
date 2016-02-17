803930907e3db1fbd56ffae6b5ac2375097a4ff2
Cyril Plisko <cyril.plisko@mountall.com>
2016-02-18 01:24:40 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,14 @@ fn main() {
 
     let cwd = env::current_dir().unwrap();
     for _ in walkdir::WalkDir::new(cwd)
-                   .min_depth(MIN_DEPTH)
-                   .max_depth(MAX_DEPTH)
-                   .into_iter()
-                   .filter_entry(|e| e.path().join("Cargo.toml").exists())
-                   .map(|e| e.ok().unwrap().path().to_path_buf())
-                   .map(|p| {println!("{}:", p.display()); cmd.current_dir(p).output().unwrap()})
-                   .map(|o| report_output(o)) {}
+                 .min_depth(MIN_DEPTH)
+                 .max_depth(MAX_DEPTH)
+                 .into_iter()
+                 .filter_entry(|e| e.path().join("Cargo.toml").exists())
+                 .map(|e| e.ok().unwrap().path().to_path_buf())
+                 .map(|p| {
+                     println!("{}:", p.display());
+                     cmd.current_dir(p).output().unwrap()
+                 })
+                 .map(|o| report_output(o)) {}
 }
