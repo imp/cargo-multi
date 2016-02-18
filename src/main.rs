9e1981ef329e1563c61b0ef1b3ef982750a37715
Cyril Plisko <cyril.plisko@mountall.com>
2016-02-18 11:23:13 +0200

    Small optimization

--- src/main.rs
+++ src/main.rs
@@ -45,15 +45,16 @@ fn main() {
     announce(&banner);
 
     let cwd = env::current_dir().unwrap();
-    for _ in walkdir::WalkDir::new(cwd)
-                 .min_depth(MIN_DEPTH)
-                 .max_depth(MAX_DEPTH)
-                 .into_iter()
-                 .filter_entry(|e| e.path().join("Cargo.toml").exists())
-                 .map(|e| e.ok().unwrap().path().to_path_buf())
-                 .map(|p| {
-                     println!("{}:", p.display());
-                     cmd.current_dir(p).output().unwrap()
-                 })
-                 .map(|o| report_output(o)) {}
+    walkdir::WalkDir::new(cwd)
+        .min_depth(MIN_DEPTH)
+        .max_depth(MAX_DEPTH)
+        .into_iter()
+        .filter_entry(|e| e.path().join("Cargo.toml").exists())
+        .map(|e| e.ok().unwrap().path().to_path_buf())
+        .map(|p| {
+            println!("{}:", p.display());
+            cmd.current_dir(p).output().unwrap()
+        })
+        .map(|o| report_output(o))
+        .last();
 }
